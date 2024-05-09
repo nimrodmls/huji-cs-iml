6983fc22b966be0d76e3dd512763ac27a20cf6c9
Nimrod Mallis <nimrodmallis@gmail.com>
2024-05-09 16:21:48 +0300

    Added files from Moodle

---
+++ ex1/house_price_prediction.py
@@ -0,0 +1,76 @@
+
+
+import pandas as pd
+
+
+def preprocess_train(X: pd.DataFrame, y: pd.Series):
+    """
+    preprocess training data.
+    Parameters
+    ----------
+    X: pd.DataFrame
+        the loaded data
+    y: pd.Series
+
+    Returns
+    -------
+    A clean, preprocessed version of the data
+    """
+    pass
+
+
+def preprocess_test(X: pd.DataFrame):
+    """
+    preprocess test data. You are not allowed to remove rows from X, but only edit its columns.
+    Parameters
+    ----------
+    X: pd.DataFrame
+        the loaded data
+
+    Returns
+    -------
+    A preprocessed version of the test data that matches the coefficients format.
+    """
+    return
+
+
+def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
+    """
+    Create scatter plot between each feature and the response.
+        - Plot title specifies feature name
+        - Plot title specifies Pearson Correlation between feature and response
+        - Plot saved under given folder with file name including feature name
+    Parameters
+    ----------
+    X : DataFrame of shape (n_samples, n_features)
+        Design matrix of regression problem
+
+    y : array-like of shape (n_samples, )
+        Response vector to evaluate against
+
+    output_path: str (default ".")
+        Path to folder in which plots are saved
+    """
+    pass
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("house_prices.csv")
+    X, y = df.drop("price", axis=1), df.price
+
+    # Question 2 - split train test
+
+    # Question 3 - preprocessing of housing prices train dataset
+
+    # Question 4 - Feature evaluation of train dataset with respect to response
+
+    # Question 5 - preprocess the test data
+
+    # Question 6 - Fit model over increasing percentages of the overall training data
+    # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
+    #   1) Sample p% of the overall training data
+    #   2) Fit linear model (including intercept) over sampled set
+    #   3) Test fitted model over test set
+    #   4) Store average and variance of loss over test set
+    # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
+
---
+++ ex1/linear_regression.py
@@ -0,0 +1,87 @@
+
+import numpy as np
+from typing import NoReturn
+
+
+class LinearRegression:
+    """
+    Linear Regression Estimator
+
+    Solving Ordinary Least Squares optimization problem
+
+    Attributes
+    ----------
+    fitted_ : bool
+        Indicates if estimator has been fitted. Set to True in ``self.fit`` function
+
+    include_intercept_: bool
+        Should fitted model include an intercept or not
+
+    coefs_: ndarray of shape (n_features,) or (n_features+1,)
+        Coefficients vector fitted by linear regression. To be set in
+        `LinearRegression.fit` function.
+    """
+
+    def __init__(self, include_intercept: bool = True):
+        """
+        Instantiate a linear regression estimator
+
+        Parameters
+        ----------
+        include_intercept: bool, default=True
+            Should fitted model include an intercept or not
+        """
+        pass
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        Fit Least Squares model to given samples
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to fit an estimator for
+
+        y : ndarray of shape (n_samples, )
+            Responses of input data to fit to
+
+        Notes
+        -----
+        Fits model with or without an intercept depending on value of `self.include_intercept_`
+        """
+        pass
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict responses for given samples using fitted estimator
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to predict responses for
+
+        Returns
+        -------
+        responses : ndarray of shape (n_samples, )
+            Predicted responses of given samples
+        """
+        pass
+
+    def loss(self, X: np.ndarray, y: np.ndarray) -> float:
+        """
+        Evaluate performance under **mean squared error (MSE) loss function**
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Test samples
+
+        y : ndarray of shape (n_samples, )
+            True labels of test samples
+
+        Returns
+        -------
+        loss : float
+            Performance under MSE loss function
+        """
+        pass
